3db478f8fc1d0bc3aa5adf5b3ad0b742221ed65c
Ali Ajeli Lahiji <mr.ali.ajeli@hotmail.com>
2023-02-11 15:29:28 +0330

    Initialize project

---
+++ src/app/head.tsx
@@ -0,0 +1,13 @@
+export default function Head() {
+	return (
+		<>
+			<title>Ali Ajeli Lahiji - Visual Studio Code</title>
+			<meta content="width=device-width, initial-scale=1" name="viewport" />
+			<meta
+				name="description"
+				content="My Portfolio themed as Visual Studio Code"
+			/>
+			<link rel="icon" href="/favicon.ico" />
+		</>
+	);
+}
